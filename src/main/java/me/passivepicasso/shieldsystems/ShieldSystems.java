3f806e6918b5d779cbf3e8a9b3846ea5d8c11001
alron <alron@bloodmagic.com>
2011-03-24 01:47:15 -0700

    Real tabs are the devil. Changed to spaces. Added author field in plugin.yml. Added a logger method to make logs look better and be a bit more informational. Broke plugin event registration out into its own method. Overrode a few bukkit methods that needed it. Moved plugin variable initialization from onEnable into onLoad, as it should be. Changed level that we listen for login/out events to Monitor, as we really should work from the final decision as we don't cancel the event.

--- src/main/java/me/passivepicasso/shieldsystems/ShieldSystems.java
+++ src/main/java/me/passivepicasso/shieldsystems/ShieldSystems.java
@@ -1,74 +1,161 @@
 package me.passivepicasso.shieldsystems;
 
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Priority;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
+// TODO: Auto-generated Javadoc
 /**
- * Spellbooks for Bukkit
- * 
+ * ShieldSystems for Bukkit.
+ *
  * @author PassivePicasso
  */
 public class ShieldSystems extends JavaPlugin {
-	public final ShieldSystemsPlayerListener	playerListener	= new ShieldSystemsPlayerListener(this);
-	public final ShieldSystemsBlockListener	blockListener	= new ShieldSystemsBlockListener(this);	; // =
-	private final HashMap<Player, Boolean>		debugees			= new HashMap<Player, Boolean>();
+    
+    /** The player listener. */
+    public final ShieldSystemsPlayerListener playerListener	= new ShieldSystemsPlayerListener(this);
+    
+    /** The block listener. */
+    public final ShieldSystemsBlockListener blockListener = new ShieldSystemsBlockListener(this);
+    
+    /** The debugees. */
+    private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
 
-	public static BukkitScheduler					scheduler		= null;
-	public static ShieldSystems					plugin;
+    /** The scheduler. */
+    public static BukkitScheduler scheduler = null;
+    
+    /** The plugin. */
+    public static ShieldSystems plugin = null;
+    
+    /** The log. */
+    private static Logger log = null;
 
-	public ShieldSystems() {
+    /**
+     * Instantiates a new shield systems.
+     */
+    public ShieldSystems() {
 
-	}
+    }
 
-	public void connectPlayer( Player player ) {
-		this.playerListener.addPlayer(player);
-	}
+    /**
+     * Connect player.
+     *
+     * @param player the player
+     */
+    public void connectPlayer( Player player ) {
+        this.playerListener.addPlayer(player);
+    }
 
-	public void disconnectPlayer( Player player ) {
-		this.playerListener.removePlayer(player);
-	}
+    /**
+     * Disconnect player.
+     *
+     * @param player the player
+     */
+    public void disconnectPlayer( Player player ) {
+        this.playerListener.removePlayer(player);
+    }
 
-	public boolean isDebugging( final Player player ) {
-		if (this.debugees.containsKey(player)) {
-			return this.debugees.get(player);
-		} else {
-			return false;
-		}
-	}
+    /**
+     * Checks if is debugging.
+     *
+     * @param player the player
+     * @return true, if is debugging
+     */
+    public boolean isDebugging( final Player player ) {
+        if (this.debugees.containsKey(player)) {
+            return this.debugees.get(player);
+        } else {
+            return false;
+        }
+    }
+    
+    /* (non-Javadoc)
+     * @see org.bukkit.plugin.Plugin#onDisable()
+     */
+    @Override
+    public void onDisable() {
+        prettyLog(Level.INFO,true,"Shutdown Completed.");
+    }
+    
+    /* (non-Javadoc)
+     * @see org.bukkit.plugin.Plugin#onEnable()
+     */
+    @Override
+    public void onEnable() {
+        registerEvents();
+        prettyLog(Level.INFO,true,"Load Completed.");
+    }
 
-	public void onDisable() {
-		System.out.println("Goodbye world!");
-	}
+    /* (non-Javadoc)
+     * @see org.bukkit.plugin.java.JavaPlugin#onLoad()
+     */
+    @Override
+    public void onLoad() {
+        log = this.getServer().getLogger();
+        plugin = this;
+        scheduler = this.getServer().getScheduler();
+        PluginDescriptionFile pdfFile = this.getDescription();
+        prettyLog(Level.INFO,true, pdfFile.getAuthors() + "Load Beginning.");
+    }
 
-	public void onEnable() {
-		plugin = this;
-
-		PluginManager pm = this.getServer().getPluginManager();
-
-		scheduler = this.getServer().getScheduler();
-		pm.registerEvent(Event.Type.BLOCK_PLACE, this.blockListener, Priority.Normal, this);
-		pm.registerEvent(Event.Type.PLAYER_JOIN, this.playerListener, Priority.Normal, this);
-		pm.registerEvent(Event.Type.PLAYER_QUIT, this.playerListener, Priority.Normal, this);
-		pm.registerEvent(Event.Type.PLAYER_INTERACT, this.playerListener, Priority.Normal, this);
-		pm.registerEvent(Event.Type.PLAYER_MOVE, this.playerListener, Priority.Normal, this);
-		pm.registerEvent(Event.Type.BLOCK_DAMAGE, this.blockListener, Priority.Normal, this);
-		pm.registerEvent(Event.Type.BLOCK_DAMAGE, this.blockListener, Priority.Normal, this);
-		// PluginDescriptionFile pdfFile = this.getDescription();
-		// System.out.println(pdfFile.getName() + " version " +
-		// pdfFile.getVersion() + " is enabled!");
-	}
-
-	@Override
-	public void onLoad() {
-	}
-
-	public void setDebugging( final Player player, final boolean value ) {
-		this.debugees.put(player, value);
-	}
+    /**
+     * Register events.
+     */
+    private void registerEvents() {
+        PluginManager pm = this.getServer().getPluginManager();
+        
+        pm.registerEvent(Event.Type.BLOCK_PLACE, this.blockListener, Priority.Normal, this);
+        pm.registerEvent(Event.Type.BLOCK_DAMAGE, this.blockListener, Priority.Normal, this);
+        pm.registerEvent(Event.Type.BLOCK_DAMAGE, this.blockListener, Priority.Normal, this); 
+        
+        // Listen for Player Joins and Quits at Monitor level. This way we are seeing the final
+        // decision of all other plugins on login and logout events.
+        pm.registerEvent(Event.Type.PLAYER_JOIN, this.playerListener, Priority.Monitor, this);
+        pm.registerEvent(Event.Type.PLAYER_QUIT, this.playerListener, Priority.Monitor, this);
+        
+        pm.registerEvent(Event.Type.PLAYER_INTERACT, this.playerListener, Priority.Normal, this);
+        pm.registerEvent(Event.Type.PLAYER_MOVE, this.playerListener, Priority.Normal, this);
+    }
+    
+    /**
+     * Sets the debugging.
+     *
+     * @param player the player
+     * @param value the value
+     */
+    public void setDebugging( final Player player, final boolean value ) {
+        this.debugees.put(player, value);
+    }
+    
+    /**
+     * 
+     * prettyLog: A quick and dirty way to make log output clean, unified, and with versioning as needed.
+     * 
+     * @param severity Level of severity in the form of INFO, WARNING, SEVERE, etc.
+     * @param version true causes version display in log entries.
+     * @param message to prettyLog.
+     * 
+     */
+    public void prettyLog(Level severity, boolean version, String message) {
+        final String prettyName = (String)("[" + this.getDescription().getName() + "]");
+        final String prettyVersion = (String)("[v" + this.getDescription().getVersion() + "]");
+        String prettyLogLine = prettyName;
+        if (version)
+        {
+            prettyLogLine += prettyVersion;
+            log.log(severity,prettyLogLine + message);
+        } 
+        else
+        {
+            log.log(severity,prettyLogLine + message);
+        }
+    }
 }
